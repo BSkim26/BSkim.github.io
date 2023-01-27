939fcecac8ccba3802047a194c8eed4d6bd176f1
kim <kyda0206@naver.com>
2023-01-27 16:26:19 +0900

    CNN2

---
+++ CNN/js/square.js
@@ -0,0 +1,38 @@
+var v1=2;
+var v2=2;
+
+square_1=square(v1, v2);
+
+function load(a1, a2){
+  var v1=parseInt(a1);
+  var v2=parseInt(a2);
+  square_1=square(v1, v2);
+}
+
+function square(a1, a2) {
+    var tag = "<table border='1'>";
+    var b1 = parseInt(a1);
+    var b2 = parseInt(a2);
+    for (j = 1; j <= b1; j++) {
+      tag += "<tr>";
+      for (i = 1; i <= b2; i++) {
+        tag += "<td>"+j+"."+i+"</td>";
+      }
+      tag += "</tr>";
+    }
+    tag += "</table>";
+    area.innerHTML = tag;
+    
+    var tag2 = "<table frame=void>";
+    for (j = 1; j <= b1*b2; j++) {
+      tag2 += "<tr>";
+      
+        tag2 += "<td>"+"O"+"</td>";
+      
+      tag2 += "</tr>";
+    }
+    tag2 += "</table>";
+    area2.innerHTML = tag2;
+  }
+
+  
